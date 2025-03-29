e6c4520da58cfcbbb64ccb1dd0e3c200e953f415
Dev-XMaghraby <xmaghrabyofficial@gmail.com>
2025-03-29 07:53:26 +0200

    Update .gitignore and remove tracked build files

--- dist/install.sh
+++
@@ -1,8 +0,0 @@
-pkg install nodejs-lts wget git
-cd ~
-mkdir Maghraby
-cd Maghraby
-wget https://github.com/Dev-XMaghraby/MG-WhatsApp-Server/releases/download/Test/latest.zip
-unzip latest.zip
-rm latest.js
-node index.js
